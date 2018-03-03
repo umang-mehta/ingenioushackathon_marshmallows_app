ec1f1bae8f17b7c36d754760d2bad95cce93ddc4
umang-mehta <36546651+umang-mehta@users.noreply.github.com>
2018-03-03 12:50:39 +0530

    Version 1.01

--- app/src/main/java/com/example/me/inghack/MainActivity.java
+++ app/src/main/java/com/example/me/inghack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.me.inghack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent moveToAuth = new Intent(MainActivity.this, AuthenticationActivity.class);
+                startActivity(moveToAuth);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
